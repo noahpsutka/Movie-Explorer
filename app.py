85c8e5b77ca332502cf746db13804183c55c54fb
Noah Psutka <noahpsutka@gmail.com>
2023-03-21 05:20:32 -0500

    Added TMDB API functionality to Flask App

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 import flask
+import requests
+import random
 import os
 from dotenv import load_dotenv, find_dotenv
 
 
 app = flask.Flask(__name__)
-load_dotenv()  # load .env for API
+load_dotenv(find_dotenv())  # load .env for API
 app.secret_key = os.getenv("APP_SECRET_KEY")
+MOV_API_BASE_URL = "https://api.themoviedb.org/3/"
+MOV_API_QUERY_PATH = "discover/movie"
 
 
 @app.route("/")
 def index():
-    return flask.render_template("index.html")
+    movie_title = get_trending_movie()
+    return flask.render_template("index.html", name=movie_title)
+
+
+def get_trending_movie():
+    mov_response = requests.get(
+        MOV_API_BASE_URL + MOV_API_QUERY_PATH,
+        params={
+            "api_key": os.getenv("TMDB_API_KEY"),
+        },
+    )
+    movies_list = mov_response.json()["results"]
+    return movies_list[random.randint(0, 10)]["title"]
 
 
 app.run(debug=True)
